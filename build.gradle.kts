7b4b20a98f8aed3abf6226787ebf93f7fe80f727
Dominic H <75951207+DQMME@users.noreply.github.com>
2022-08-14 13:08:52 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+val javaVersion = 17
+val kspigotVersion = "1.19.0"
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("io.papermc.paperweight.userdev") version "1.3.8"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("xyz.jpenilla.run-paper") version "1.0.6"
+}
+
+group = "org.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // PaperMC Dependency
+    paperDevBundle("1.19.2-R0.1-SNAPSHOT")
+
+    // KSpigot dependency
+    implementation("net.axay", "kspigot", kspigotVersion)
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf(
+                "-Xjdk-release=$javaVersion",
+            )
+            jvmTarget = "$javaVersion"
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(javaVersion)
+    }
+    assemble {
+        dependsOn(reobfJar)
+    }
+}
+
+bukkit {
+    name = "ExamplePlugin"
+    apiVersion = "1.19"
+    authors = listOf(
+        "Your Name",
+    )
+    main = "$group.exampleplugin.ExamplePlugin"
+    version = getVersion().toString()
+    libraries = listOf(
+        "net.axay:kspigot:$kspigotVersion",
+    )
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "exampleplugin"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        // PaperMC Repo
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
